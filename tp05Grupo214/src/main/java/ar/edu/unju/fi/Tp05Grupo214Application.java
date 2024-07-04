7be008dbaf4790b87ccf1fb5cbc307f060fce47e
Kevin <Kevin@joelcruz>
2024-07-04 15:44:25 -0300

    Creacion del proyecto y la finalizacion del Index

---
+++ tp05Grupo214/src/main/java/ar/edu/unju/fi/Tp05Grupo214Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp05Grupo214Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp05Grupo214Application.class, args);
+	}
+
+}
